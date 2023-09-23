7ebfdaf275863021ccc748b7d8c9c713fa54ed7a
heyakash876 <119721777+heyakash876@users.noreply.github.com>
2023-09-23 21:06:54 +0530

    Create erc20.sol

---
+++ erc20.sol
@@ -0,0 +1,54 @@
+pragma solidity ^0.8.0;
+
+contract MyToken {
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    uint256 public totalSupply;
+    address public owner;
+
+    mapping (address => uint256) public balanceOf;
+    mapping (address => mapping (address => uint256)) public allowance;
+
+    constructor(string memory _name, string memory _symbol, uint8 _decimals, uint256 _initialSupply) {
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimals;
+        totalSupply = _initialSupply;
+        balanceOf[msg.sender] = totalSupply;
+        owner = msg.sender;
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        allowance[_from][msg.sender] -= _value;
+        return true;
+    }
+
+    function mint(uint256 amount) public {
+        require(msg.sender == owner);
+        totalSupply += amount;
+        balanceOf[owner] += amount;
+    }
+
+    function burn(uint256 amount) public {
+        require(balanceOf[msg.sender] >= amount);
+        totalSupply -= amount;
+        balanceOf[msg.sender] -= amount;
+    }
+}
